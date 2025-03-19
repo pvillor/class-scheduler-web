735034b27b33d5f89a763e1ed0e020492358e70f
paulo.guerra <paulo.guerra@flex-am.com.br>
2025-03-19 12:13:42 -0400

    config: env, query client and axios

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Toaster } from 'sonner'
 
 import { router } from './routes'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -13,7 +15,9 @@ export function App() {
       <ThemeProvider defaultTheme="dark" storageKey="classscheduler-theme">
         <Toaster richColors />
         <Helmet titleTemplate="%s | class.scheduler" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
