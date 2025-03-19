1ccba6ed228ba2a3ac68c38fd65408b50a0fbe0a
paulo.guerra <paulo.guerra@flex-am.com.br>
2025-03-18 20:33:04 -0400

    config: versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'node:path'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
